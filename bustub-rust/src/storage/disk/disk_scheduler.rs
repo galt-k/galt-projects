5bf5493d3fd54edb68845675a83500981ea6779e
galt-k <revanth.katanguri@gmail.com>
2025-04-20 14:41:18 -0700

    target/debug default changes

--- bustub-rust/src/storage/disk/disk_scheduler.rs
+++ bustub-rust/src/storage/disk/disk_scheduler.rs
@@ -9,19 +9,20 @@ use std::sync::Mutex;
 pub struct DiskScheduler {
     pub disk_manager: DiskManager,
     pub request_queue_tx: Arc<Mutex<Sender<Option<DiskRequest>>>>,
-    pub request_queue_rx: Receiver<Option<DiskRequest>>,
-    pub background_thread: Option<thread::JoinHandle<()>>, // handle to a spawned thread
+    pub request_queue_rx: Arc<Mutex<Receiver<Option<DiskRequest>>>>, // making the receiver a mutex
+    pub background_threads: Vec<thread::JoinHandle<()>>, // handle to a spawned thread
 }
 
 impl DiskScheduler {
     pub fn new(disk_manager: DiskManager) -> Self {
         let (tx, rx) = channel();
+        let rx = Arc::new(Mutex::new(rx)); // putting the receiver into a mutex
          
         let mut scheduler = Self {
             disk_manager,
             request_queue_tx: Arc::new(Mutex::new(tx)),
             request_queue_rx: rx,
-            background_thread: None
+            background_threads: Vec::new()
         };
         scheduler.start_worker_thread();
         scheduler
@@ -34,28 +35,50 @@ impl DiskSchedulerTrait for DiskScheduler {
     fn schedule(&self, disk_request: DiskRequest) {
         self.request_queue_tx.lock().unwrap().send(Some(disk_request)).unwrap();
     }
+
     fn start_worker_thread(&mut self) {
-        if self.background_thread.is_none() {
+        if self.background_threads.len() == 0 {
+            let max_num_threads = 10;
             // start only if the thread is not running
-            let dm = self.disk_manager.clone();
-            let rx = std::mem::replace(&mut self.request_queue_rx, channel().1); // Move rx—replace with dummy
+            let dm = Arc::new(self.disk_manager.clone()); //Arc for sharing across multiple threads
+            //let rx: Receiver<Option<DiskRequest>> = std::mem::replace(&mut self.request_queue_rx, channel().1); // Move rx—replace with dummy
+            //let rx_new: std::sync::MutexGuard<'_, Receiver<Option<DiskRequest>>> = self.request_queue_rx.lock().unwrap();
+            let clone = self.request_queue_rx.clone();
+            let dm_clone = dm.clone();
             
-            let handle = thread::spawn(move || {
-                while let Ok(Some(req)) = rx.recv() {
-                    if req.is_write {
-                        let data = req.data.lock().unwrap();
-                        dm.write_page(req.page_id, &data);
-                    } else {
-                        let mut data = req.data.lock().unwrap();
-                        dm.read_page(req.page_id, &mut data);
+
+            
+
+
+            for _ in 0..max_num_threads {
+                let clone = clone.clone();
+                let dm_clone = dm_clone.clone();
+
+                let handle = thread::spawn(move || {
+
+                    // new change
+                    
+                    let rx_new: std::sync::MutexGuard<'_, Receiver<Option<DiskRequest>>> = clone.lock().unwrap();
+                    //
+                    while let Ok(Some(req)) = rx_new.recv() {
+                        if req.is_write {
+                            let data = req.data.lock().unwrap();
+                            dm_clone.write_page(req.page_id, &data);
+                        } else {
+                            let mut data = req.data.lock().unwrap();
+                            dm_clone.read_page(req.page_id, &mut data);
+                        }
+                        req.is_done.send(true).unwrap();
                     }
-                    req.is_done.send(true).unwrap();
-                }
-            });
-            unsafe {
-                let mt = self as *const Self as *mut Self;
-                (*mt).background_thread = Some(handle);
+                });
+                self.background_threads.push(handle)
+
             }
+            
+            // unsafe {
+            //     let mt = self as *const Self as *mut Self;
+            //     (*mt).background_threads = Some(handle);
+            // }
         }
     }
 
--- bustub-rust/tests/disk_scheduler_test.rs
+++ bustub-rust/tests/disk_scheduler_test.rs
@@ -25,34 +25,46 @@ mod tests {
     fn test_disk_read_page() {
         let dm = DiskManager::new("test.db").unwrap();
         let mut scheduler = DiskScheduler::new(dm);
-        let (tx,rx) = channel(); 
-        // write some data
-        let write_data = vec![0xFF;4096];
+        let (tx, rx) = channel();
+    
+        // Write
+        let write_data = vec![0xFF; 4096];
+        let write_data2 = vec![0xFF; 4096];
         let write_req = DiskRequest {
             page_id: 1,
-            //wrapping the write_data inside a thread safe shared ownership mechanism
-            // Creating a Ref-counted, thread-safe, mutable resource
             data: Arc::new(Mutex::new(write_data)),
             is_write: true,
-            is_done: tx.clone()
+            is_done: tx.clone(),
+        };
+        let write_req2 = DiskRequest {
+            page_id: 2,
+            data: Arc::new(Mutex::new(write_data2)),
+            is_write: true,
+            is_done: tx.clone(),
         };
         scheduler.schedule(write_req);
+        scheduler.schedule(write_req2);
         rx.recv().unwrap();
-
+        rx.recv().unwrap();
+    
+        // 10 Parallel Reads
         let read_data = Arc::new(Mutex::new(vec![0; 4096]));
-        let read_req = DiskRequest {
-            page_id: 1,
-            data: read_data.clone(),
-            is_write: false,
-            is_done: tx,
-        };
-        scheduler.schedule(read_req);
-        rx.recv().unwrap();
-
+        for _ in 0..10 {
+            let read_req = DiskRequest {
+                page_id: 1,
+                data: read_data.clone(),
+                is_write: false,
+                is_done: tx.clone(),
+            };
+            scheduler.schedule(read_req);
+        }
+        for _ in 0..10 {
+            rx.recv().unwrap(); // Wait for all
+        }
+    
         let result = read_data.lock().unwrap();
-        assert_eq!(result.len(), 4096,"Read data length matched");
-        assert_eq!(&result[..], &[0xFF; 4096], "Content match")
-
+        assert_eq!(result.len(), 4096, "Read data length matched");
+        assert_eq!(&result[..], &[0xFF; 4096], "Content match");
     }
 
     // Add more test cases here as needed
