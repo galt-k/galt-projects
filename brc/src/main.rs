cf1ab0aef6641ed1731a472ec574f711a0030925
galt-k <revanth.katanguri@gmail.com>
2026-01-10 10:27:26 -0800

    parsing temperature manually

--- brc/src/main.rs
+++ brc/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let f = File::open("measurements.txt").unwrap();
     let map = mmap(&f);
     // initializing a hashmap
-    let mut stats = HashMap::<Vec<u8>, (f64, f64, usize, f64)>::new();
+    let mut stats = HashMap::<Vec<u8>, (i16, i64, usize, i16)>::new();
 
     for line in map.split(|c| *c == b'\n') {
         if line.is_empty() {
@@ -20,22 +20,18 @@ fn main() {
                 std::str::from_utf8_unchecked(line)
             });
         };
-        //utf8 unchecked is being used bcz, read me file specified that 
-        // input file is utf-8 encoded. 
-        let temperature: f64 = unsafe { std::str::from_utf8_unchecked(temperature) }
-            .parse()
-            .unwrap();        
-        // removing the allocation of a new string
+        
+        let t = parse_temperature(temperature);
         let stats = match stats.get_mut(station) {
             Some(stats) => stats,
             None => stats
                 .entry(station.to_vec())
-                .or_insert((f64::MAX, 0., 0, f64::MIN)),
+                .or_insert((i16::MAX, 0, 0, i16::MIN)),
         };
-        stats.0 = stats.0.min(temperature);
-        stats.1 += temperature;
+        stats.0 = stats.0.min(t);
+        stats.1 += i64::from(t);
         stats.2 += 1;
-        stats.3 = stats.3.max(temperature);
+        stats.3 = stats.3.max(t);
     }
     print!("{{");
     let stats = BTreeMap::from_iter(
@@ -45,7 +41,11 @@ fn main() {
     );
     let mut stats = stats.into_iter().peekable();
     while let Some((station, (min, sum, count, max))) = stats.next() {
-        print!("{station}={min:.1}/{:.1}/{max:.1}", sum / (count as f64));
+        print!("{station}={:.1}/{:.1}/{:.1}", 
+            (min as f64)/10.,
+            (sum as f64)/10. / (count as f64),
+            (max as f64)/10.
+        );
         if stats.peek().is_some() {
             print!(", ");
         }
@@ -80,4 +80,26 @@ fn mmap(f: &File) -> &'_ [u8] {
         }
 
     }
+}
+
+fn parse_temperature(temperature: &[u8])-> i16 {
+    let mut t : i16 = 0;
+    let mut mul = 1;
+    for &d in temperature.iter().rev() {
+        match d {
+            b'.' => {
+                continue;
+            }
+            b'-' => {
+                t = -t;
+                break;
+            }
+            _ => {
+                t += i16::from(d - b'0') * mul;
+                mul *= 10;
+            }
+        }
+
+    }
+    t
 }
